02d377a340d96afb45358ba513e761a5ab9904cc
savitrasapre <savitrasapre@gmail.com>
2017-08-29 11:31:16 -0400

    Adding MissingNumber and learnt about XOR trick with 0 and number itself.

---
+++ LeetCodeSolutions/MissingNumber.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include "LeetcodeUtility.h"
+
+/*
+The idea being that XOR of number with itself is 0 and XOR with 0 is the same number.
+ 0 2 3 4 5
+ 0 1 2 3 4            5
+*/
+
+int missingNumber(std::vector<int>& nums)
+{
+	int count = 0;
+	for (int i = 0; i < nums.size(); i++)
+		count ^= i ^ nums[i];
+	return count ^ nums.size();
+}
+
+int main()
+{
+	std::vector<int> input { 0, 2, 3, 4, 5 };
+	std::cout << missingNumber(input);
+	return 0;
+}
+
+
