bf57cdf58c61486eb5f88668501298c24eab7d60
savitrasapre <savitrasapre@gmail.com>
2017-09-06 21:18:02 -0400

    Modified class to make it templated LinkedList

--- LeetCodeSolutions/LinkedList.cpp
+++ LeetCodeSolutions/LinkedList.cpp
@@ -4,13 +4,19 @@
 
 int main()
 {
-	LinkedList list;
-	list.insert(15);
-	list.insert(14);
-	list.insert(13);
-	list.insert(12);
-	std::cout << list.size;
-	list.display();
+	LinkedList<int> list1;
+	list1.insert(15);
+	list1.insert(14);
+	list1.insert(13);
+	list1.insert(12);
+	list1.insert(12);
+	LinkedList<char> list2;
+	list2.insert('a');
+	list2.insert('b');
+	std::cout << list2.size;
+	list2.display();
+	std::cout << list1.size;
+	list1.display();
 
 	return 0;
 
