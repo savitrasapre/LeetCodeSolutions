7ff49ae9426ff58e20faf174d7ca9a6ab7e6b382
savitrasapre <savitrasapre@gmail.com>
2017-09-07 14:38:48 -0400

    Added reverse(). Used 3 pointers to keep track or prev,current and next node

--- LeetCodeSolutions/LinkedList.h
+++ LeetCodeSolutions/LinkedList.h
@@ -8,6 +8,7 @@ public:
 	~LinkedList();
 	void insert(T val);
 	void display();
+	void reverse();
 	int size = 0;
 private:
 	struct node
@@ -17,16 +18,19 @@ private:
 	};
 	struct node* head = NULL;
 };
+
 template<typename T>
 LinkedList<T>::LinkedList()
 {
 
 }
+
 template<typename T>
 LinkedList<T>::~LinkedList()
 {
 
 }
+
 template<typename T>
 void LinkedList<T>::insert(T value)
 {
@@ -50,14 +54,38 @@ void LinkedList<T>::insert(T value)
 	}
 	LinkedList::size++;
 }
+
 template<typename T>
 void LinkedList<T>::display()
 {
 	node* temp = head;
 	std::cout << "Elements of LinkedList are: " << "\n";
-	while (temp != NULL)
+	while (temp != nullptr)
 	{
 		std::cout << temp->data<<"\n";
 		temp = temp->next;
 	}
+}
+
+template<typename T>
+void LinkedList<T>::reverse()
+{	
+	/* The idea here is to def another prev pointer which acts as a lazy pointer which stores the address of the previous node
+		We also want to move the current pointer to the next node. But, we have already changed the link to point to the previous node
+		and cannot go ahead untill we store the value in another pointer temp.
+	*/
+
+	node* current = head;
+	node* prev = nullptr;
+	node* temp;
+
+	while (current != nullptr)
+	{
+		temp = current->next;
+		current->next = prev;
+		prev = current;
+		current = temp;
+	}
+	head = prev;
+	
 }
