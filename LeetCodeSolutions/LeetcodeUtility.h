a5692b2ae0e1b6293e06876901ceebbb9df6190b
savitrasapre <savitrasapre@gmail.com>
2017-12-14 19:41:39 -0500

    Overloaded printArray() to take simple arrays as args as well.

--- LeetCodeSolutions/LeetcodeUtility.h
+++ LeetCodeSolutions/LeetcodeUtility.h
@@ -8,6 +8,18 @@ namespace utility
 			for (T i : arr)
 				std::cout << i << "\n";
 		}
+	
+	template<typename T>
+	void printArray(T& arr,int size)
+	{
+		std::cout << "Elements are :" << "\n";
+		int i = 0;
+		while (i < size)
+		{
+			std::cout << arr[i] << "\n";
+			i++;
+		}
+	}
 }
 
 
